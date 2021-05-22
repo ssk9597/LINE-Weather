6903857f3fe56edc913db6ec0da04090efa54a96
sasaki <skyo.97.admj@gmail.com>
2021-05-22 15:44:18 +0900

    Add import_env

--- src/app/Http/Controllers/LINEController.php
+++ src/app/Http/Controllers/LINEController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class LINEController extends Controller
 {
-    //
+  public function sendMessage(Request $request)
+  {
+    // env
+    $channelSecret = env("LINE_CHANNEL_SECRET");
+    $channelAccessToken = env("LINE_CHANNEL_ACCESS_TOKEN");
+  }
 }
