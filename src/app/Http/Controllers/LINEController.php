52866862106c8aa022cdbb480194d9212ea4e420
sasaki <skyo.97.admj@gmail.com>
2021-05-22 15:49:56 +0900

    Add verification_signature

--- src/app/Http/Controllers/LINEController.php
+++ src/app/Http/Controllers/LINEController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// LINE
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\SignatureValidator;
+
 
 class LINEController extends Controller
 {
@@ -11,5 +15,11 @@ class LINEController extends Controller
     // env
     $channelSecret = env("LINE_CHANNEL_SECRET");
     $channelAccessToken = env("LINE_CHANNEL_ACCESS_TOKEN");
+
+    // 署名を検証する
+    $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
+    if (!SignatureValidator::validateSignature($request->getContent(), $channelSecret, $signature)) {
+      return;
+    }
   }
 }
