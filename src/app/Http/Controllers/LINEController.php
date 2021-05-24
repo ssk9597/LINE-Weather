9a5db0547273d59ccd9f41f7aa6b2085f98722e0
sasaki <skyo.97.admj@gmail.com>
2021-05-24 17:25:25 +0900

    Update self_made_function_button_messages

--- src/app/Http/Controllers/LINEController.php
+++ src/app/Http/Controllers/LINEController.php
@@ -43,6 +43,11 @@ class LINEController extends Controller
     //Webhookの処理
     $events = $bot->parseEventRequest($request->getContent(), $signature);
 
+    // text
+    $btn_text = "現在地を送る";
+    $btn_url = "https://line.me/R/nv/location/";
+    $btn_builder = "現在地を送ってください";
+
     foreach ($events as $event) {
       // イベントのリプライトークンを取得する
       $replyToken = $event->getReplyToken();
@@ -55,19 +60,13 @@ class LINEController extends Controller
         // 入力された文字が「今日の洋服は？」「明日の洋服は？」かどうかで応答メッセージを変更する
         if ($message === "今日の洋服は？") {
           // text
-          $btn_text = "現在地を送る";
-          $btn_url = "https://line.me/R/nv/location/";
           $btn_message = "今日はどんな洋服にしようかな";
-          $btn_builder = "現在地を送ってください";
           // class
           ButtonMessages::createButtonMessage($bot, $replyToken, $btn_text, $btn_url, $btn_message, $btn_builder);
         } else if ($message === "明日の洋服は？") {
           $isToday = false;
           // text
-          $btn_text = "現在地を送る";
-          $btn_url = "https://line.me/R/nv/location/";
           $btn_message = "明日はどんな洋服にしようかな";
-          $btn_builder = "現在地を送ってください";
           // class
           ButtonMessages::createButtonMessage($bot, $replyToken, $btn_text, $btn_url, $btn_message, $btn_builder);
         } else {
