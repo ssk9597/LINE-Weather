dea73310fca3642f897d8afdbd533e07b470521a
sasaki <skyo.97.admj@gmail.com>
2021-05-24 17:22:51 +0900

    Update self_made_function_button_messages

--- src/config/app.php
+++ src/config/app.php
@@ -226,6 +226,7 @@ return [
     'Validator' => Illuminate\Support\Facades\Validator::class,
     'View' => Illuminate\Support\Facades\View::class,
     "FlexMessages" => App\Library\FlexMessages::class,
+    "ButtonMessages" => App\Library\ButtonMessages::class,
   ],
 
 ];
